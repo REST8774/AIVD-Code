ad1bb99f422e1695e331a813566198d598eed27e
GomezvLR74 <GomezvLR74@roseville.k12.mi.us>
2024-02-07 21:58:16 -0500

    whatever

---
+++ AIVD.ino
@@ -0,0 +1,23 @@
+int frontLeftTrig = A0;
+int frontLeftEcho = A1;
+
+int frontRightTrig = A2;
+int frontRightEcho = A3;
+
+int backLeftTrig = A4;
+int backLeftEcho = A5;
+
+int backRightTrig = A6;
+int backRightEcho = A7;
+
+int rightFrontTrig = A8;
+int rightFrontEcho = A9;
+
+int rightBackTrig = A10;
+int rightBackEcho = A11;
+void setup(){
+
+}
+void loop(){
+
+}
