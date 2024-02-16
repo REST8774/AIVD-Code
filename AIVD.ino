833e9f7bc66e34be44b745610c05b44bcb0b83fb
GomezvLR74 <GomezvLR74@roseville.k12.mi.us>
2024-02-16 17:55:24 -0500

    so

--- AIVD.ino
+++ AIVD.ino
@@ -1,23 +1,56 @@
-int frontLeftTrig = A0;
-int frontLeftEcho = A1;
+//included libraries
+#include <Servo.h>
 
-int frontRightTrig = A2;
-int frontRightEcho = A3;
+//motors
+Servo driveMotor;
+Servo steerServo;
 
-int backLeftTrig = A4;
-int backLeftEcho = A5;
+//sensors
+int frontLeftTrig = A1;
+int frontLeftEcho = A2;
 
-int backRightTrig = A6;
-int backRightEcho = A7;
+int frontRightTrig = A3;
+int frontRightEcho = A4;
 
-int rightFrontTrig = A8;
-int rightFrontEcho = A9;
+int backLeftTrig = A5;
+int backLeftEcho = A6;
 
-int rightBackTrig = A10;
-int rightBackEcho = A11;
-void setup(){
+int backRightTrig = A7;
+int backRightEcho = A8;
+
+int rightFrontTrig = A9;
+int rightFrontEcho = A10;
+
+int rightBackTrig = A11;
+int rightBackEcho = A12;
+
+//variables
+  int potPin = A0;
+  int potVal;
+  int servoPos;
+  double del;
+
+void setup() {
+  Serial.begin(9600);
+  steerServo.attach(5);
+  driveMotor.attach(6);
+  pinMode(potPin, INPUT);
+
+  forward(24, 0);
+}
+
+void loop() {
 
 }
-void loop(){
 
+void forward(int distance, int steer){
+  del = ((1/23.2) * distance) * 1000;
+  servoPos = map(steer, -15, 15, 47, 77);
+  steerServo.write(servoPos);
+  delay(1000);
+  driveMotor.write(100);
+  delay(1000);
+  driveMotor.write(110);
+  delay(del);
+  driveMotor.write(90);
 }
